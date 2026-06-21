e9269c432d2ec11ed7d4a1a1a83eea2ae6df2a30
Elian <aramallog.elian@gmail.com>
2026-06-21 19:19:41 -0300

    Modificado el path del archivo project.yml Primer test rojo (no compila)

--- test/test_reloj.c
+++ test/test_reloj.c
@@ -1,18 +1,20 @@
 #include "unity.h"
 #include "reloj.h"
 
-//Al inicializar, el reloj está en 00:00 y con una hora inválida.
+void setUp(void) {
+    // Esta función se ejecuta antes de cada test
+}
 
+void tearDown(void) {
+    // Esta función se ejecuta después de cada test
+}
 
-
-void test_reloj_inicial_invalido(void){
-
-    clock_t reloj;
-    hora_t hora_actual;
-    bool es_valido;
-    reloj=RelojCreate(1,NULL);
-    es_valido=RelojGetHora(reloj,hora_actual);
-    TEST_ASSERT_FALSE(es_valido);
-    TEST_ASSERT_EQUAL_UINT8_ARRAY((hora_t){0,0,0,0,0,0}, hora_actual, 6);
-
+void test_reloj_inicia_en_cero_y_estado_invalido(void) {
+    uint8_t hora_esperada[] = {0, 0, 0, 0, 0, 0};
+    uint8_t hora_actual[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}; // Lleno de basura para asegurar que la función lo sobreescribe
+    
+    clock_t reloj = reloj_crear(100); // 100 ticks por segundo
+    
+    TEST_ASSERT_FALSE(reloj_hora_es_valida(reloj));
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(hora_esperada, reloj_obtener_hora(reloj, hora_actual), 6);
 }
