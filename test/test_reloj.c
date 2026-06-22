bbd1fe24718f0b3cff3bf5f226b6bd377c6f1f15
Elian <aramallog.elian@gmail.com>
2026-06-22 15:23:49 -0300

    Se realiza el test para ajustar hora y verificar validez. Test en verde (pasó)

--- test/test_reloj.c
+++ test/test_reloj.c
@@ -25,3 +25,21 @@ void test_reloj_inicia_en_cero_y_estado_invalido(void) {
     // Verificamos que el arreglo de la hora actual quedó en 00:00:00
     TEST_ASSERT_EQUAL_UINT8_ARRAY(hora_esperada, hora_actual, 6);
 }
+
+void test_ajustar_hora_valida(void) {
+    // 1. Preparar
+    hora_t hora_esperada = {1, 2, 3, 4, 5, 6}; // Representa las 12:34:56
+    hora_t hora_actual;
+    clock_t reloj = RelojCreate(100, NULL);
+    
+    // 2. Ejecutar
+    // Ajustamos la hora usando la función de nuestra API
+    RelojSetHora(reloj, hora_esperada);
+    
+    // 3. Comprobar
+    // Verificamos que GetHora ahora devuelve true (hora válida)
+    TEST_ASSERT_TRUE(RelojGetHora(reloj, hora_actual));
+    
+    // Verificamos que el arreglo interno guardó exactamente la hora que le pasamos
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(hora_esperada, hora_actual, 6);
+}
